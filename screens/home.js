ca77be875f4e7079b262110aea263e3da0c47ed5
Davide Greco <2402884g@student.gla.ac.uk>
2021-08-31 12:31:53 +0100

    Added a global font file

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { SafeAreaView, View, Text } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { globalFont } from "../styles/globalFonts";
 
 const home = () => {
   return (
     <SafeAreaView>
-      <Text style={{ fontFamily: "noto-serif-regular" }}>Home Screen</Text>
+      <Text style={globalFont.bold}>Home Screen</Text>
     </SafeAreaView>
   );
 };
---
+++ styles/globalFonts.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const globalFont = StyleSheet.create({
+  regular: {
+    fontFamily: "noto-serif-regular",
+  },
+  bold: {
+    fontFamily: "noto-serif-bold",
+  },
+  italic: {
+    fontFamily: "noto-serif-italic",
+  },
+});
